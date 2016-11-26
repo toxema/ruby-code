a19360ad7f6bdd4cb54be107a70dd7349cd3872d
toxema <toxema@gmail.com>
2016-11-27 00:44:20 +0300

    ikinci kayit

--- src/com/yakut/util/DateUtil.java
+++ src/com/yakut/util/DateUtil.java
@@ -11,95 +11,100 @@ import java.util.logging.Logger;
  */
 public class DateUtil {
 
-            static SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
-            static SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
-            static SimpleDateFormat formatFull = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+    static SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+    static SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+    static SimpleDateFormat formatFull = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 
-            public static void uyu(int time) {
-                        try {
-                                    Thread.sleep(time);
-                        } catch (InterruptedException ex) {
-                                    Logger.getLogger(DateUtil.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-            }
+    public static void uyu(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(DateUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-            public static void sleep(int time){
-                        uyu(time);
-            }
-            public static String formatTime(Date date) {
-                        String s = null;
-                        try {
-                                    s = timeFormat.format(date);
-                        } catch (Exception ex) {
-                                    s = "HATA";
-                        }
-                        return s;
-            }
+    public static void uyu() {
+        uyu(500);
+    }
 
-            public static Date fullParseDate(String text) {
-                        Date date = null;
-                        try {
-                                    date = formatFull.parse(text);
-                        } catch (Exception ex) {
-                        }
-                        return date;
-            }
+    public static void sleep(int time) {
+        uyu(time);
+    }
 
-            public static String fullFormatDate(Date date) {
-                        if (date == null) {
-                                    return "";
-                        }
+    public static String formatTime(Date date) {
+        String s = null;
+        try {
+            s = timeFormat.format(date);
+        } catch (Exception ex) {
+            s = "HATA";
+        }
+        return s;
+    }
 
-                        return formatFull.format(date);
-            }
+    public static Date fullParseDate(String text) {
+        Date date = null;
+        try {
+            date = formatFull.parse(text);
+        } catch (Exception ex) {
+        }
+        return date;
+    }
 
-            public static String formatDate(Date date) {
-                        String s;
-                        try {
-                                    s = format.format(date);
-                        } catch (Exception ex) {
-                                    s = "HATA";
-                        }
-                        return s;
-            }
+    public static String fullFormatDate(Date date) {
+        if (date == null) {
+            return "";
+        }
 
-            public static Date parseDate(String d) {
-                        Date date = null;
-                        try {
-                                    date = format.parse(d);
-                        } catch (Exception ex) {
-                        }
-                        return date;
-            }
+        return formatFull.format(date);
+    }
 
-            public static Date parseTime(String time) {
-                        Date date = null;
-                        try {
-                                    date = timeFormat.parse(time);
-                        } catch (Exception ex) {
-                        }
-                        return date;
-            }
+    public static String formatDate(Date date) {
+        String s;
+        try {
+            s = format.format(date);
+        } catch (Exception ex) {
+            s = "HATA";
+        }
+        return s;
+    }
 
-            public static String getDate() {
-                        return formatDate(new Date());
-            }
+    public static Date parseDate(String d) {
+        Date date = null;
+        try {
+            date = format.parse(d);
+        } catch (Exception ex) {
+        }
+        return date;
+    }
 
-            public static String getTime() {
-                        return formatTime(new Date());
-            }
+    public static Date parseTime(String time) {
+        Date date = null;
+        try {
+            date = timeFormat.parse(time);
+        } catch (Exception ex) {
+        }
+        return date;
+    }
 
-            public static String getTimeStamp() {
-                        return fullFormatDate(new Date());
-            }
+    public static String getDate() {
+        return formatDate(new Date());
+    }
 
-            public static Date getGunBaslangis() {
-                        Date bas = fullParseDate(formatDate(new Date()) + " 00:00:00");
-                        return bas;
-            }
+    public static String getTime() {
+        return formatTime(new Date());
+    }
 
-            public static Date getGunBitis() {
-                        Date bas = fullParseDate(formatDate(new Date()) + " 23:59:59");
-                        return bas;
-            }
+    public static String getTimeStamp() {
+        return fullFormatDate(new Date());
+    }
+
+    public static Date getGunBaslangis() {
+        Date bas = fullParseDate(formatDate(new Date()) + " 00:00:00");
+        return bas;
+    }
+
+    public static Date getGunBitis() {
+        Date bas = fullParseDate(formatDate(new Date()) + " 23:59:59");
+        return bas;
+    }
 }
